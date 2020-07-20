f463f0684358cf089471a0b04551920ee8118258
stoneberg <stoneberg73@gmail.com>
2020-07-20 16:46:31 +0900

    Initial commit

---
+++ src/main/java/com/stone/store/play/PlayStoreApplication.java
@@ -0,0 +1,13 @@
+package com.stone.store.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stone/store/play/PlayStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.store.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
